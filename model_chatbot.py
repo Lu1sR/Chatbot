72a4df2fc0242cd3d57f87a61b98355eb146e65b
davnm5 <josdanei@espol.edu.ec>
2020-01-11 19:31:28 -0500

    [ADD] modelo básico

---
+++ model_chatbot.py
@@ -0,0 +1,31 @@
+import tflearn
+import tensorflow
+from preprocessor import *
+
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+    
+def create_model():
+    model.fit(training, output, n_epoch=3000, batch_size=8, show_metric=True)
+    model.save("model/model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
